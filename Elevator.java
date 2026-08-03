f3158ad35ad8d94246d0974cdb5d1688c1ec591a
noamkabiri <noam.kabiri@gmail.com>
2026-08-03 18:59:52 +0300

    half of iii

--- Elevator.java
+++ Elevator.java
@@ -5,12 +5,16 @@ public class Elevator {
     double power;
     boolean isup;
     boolean isdown;
-    
+    double speed;
+    double position;
+
     public Elevator(double minRange, double maxRange, double maxSpeed) {
         this.minRange = minRange;
         this.maxRange = maxRange;
         this.maxSpeed = maxSpeed;
+        this.position = position;
     }
+
     public double getMinRange() {
         return minRange;
     }
@@ -22,14 +26,44 @@ public class Elevator {
     public double getMaxSpeed() {
         return maxSpeed;
     }
-    
+
     public double getPower() {
         return power;
     }
+
     public boolean isUp() {
         return isup;
     }
+
     public boolean isDown() {
         return isdown;
     }
-}   
+
+    public double getPosition() {
+        return position;
+    }
+
+    public void periodicUpdate(double speed, double minRange, double maxRange, double position) {
+        this.speed = maxSpeed*power;
+        
+       if (position>maxRange) {
+            position = maxRange;
+            speed = 0;
+            power = 0;
+        }
+    
+
+
+       else if (position<minRange) {
+            position = minRange;
+            speed = 0;
+            power = 0;
+        }
+
+
+       else {
+            position += speed;
+        }
+        
+      } 
+}
