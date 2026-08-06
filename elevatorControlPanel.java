7f3b6e52e150226d73812637155bf5c85ccf87ba
YuvalGraninja <oyuval2012@gmail.com>
2026-08-06 09:59:04 +0300

    I belive almost complete

--- elevatorControlPanel.java
+++ elevatorControlPanel.java
@@ -1,5 +1,30 @@
+import java.util.Random;
+
 public class elevatorControlPanel {
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Elevator elevator = new Elevator(0,1.5,0.9,0);
+
+        Random random = new Random();
+
+        while(true){
+
+            double target = random.nextDouble()*1.5;
+
+            elevator.setTargetHeight(target);
+
+            while(!elevator.isAtTargetHeight()){
+
+                elevator.periodicUpdate();
+
+                System.out.println("Target: " + target);
+                System.out.println("Height: " + elevator.getPosition());
+
+                Thread.sleep(100);
+            }
+
+            Thread.sleep(2000);
+        }
     }
 }
